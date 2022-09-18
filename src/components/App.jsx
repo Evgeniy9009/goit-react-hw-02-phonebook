b120ded5336b9f717f1018ae58c451886bf89729
Evgeniy <evgeniy.grabar1@gmail.com>
2022-09-19 00:11:12 +0300

    add form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import ContactForm from "./ContactForm/ContactForm";
+
 export const App = () => {
   return (
     <div
@@ -10,7 +12,8 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <ContactForm />
+      {/* onSubmit={this.addContact} */}
     </div>
   );
 };
