f001ba0625f6d177df6c3124af645e65c5034de9
Evgeniy <evgeniy.grabar1@gmail.com>
2022-09-20 00:15:43 +0300

    css

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,10 @@
-import css from 'components/ContactList/ContactList.modale.css'
+import css from 'components/ContactList/ContactList.module.css'
 
 export default function ContactList({items , removeContact }) {
     const elements = items.map(({ name, number, id}) => {
       return (
 
-          <li key={id} > {name} {number} <button onClick={ () => {removeContact(id)} }>Delete</button></li>
+          <li className={css.item} key={id} > {name} {number} <button className={css.btn} onClick={ () => {removeContact(id)} }>Delete</button></li>
           
 
       )
